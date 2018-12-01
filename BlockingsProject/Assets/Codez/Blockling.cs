417fe5e1ad045594b292a016d9a524754e916e25
AbroadGames <AbroadGames@users.noreply.github.com>
2018-12-01 10:27:50 +0000

    Project Setup and Stub Code for Blockling AI

---
+++ BlockingsProject/Assets/Codez/Blockling.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blockling : MonoBehaviour
+{
+
+    //Vars
+    [SerializeField] private int speed = 5;
+    [SerializeField] private bool hasPlayerInfulence = false;
+    [SerializeField] private bool isSacrificed = false;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+
+    private void Moving()
+    {
+
+    }
+
+    private void Stop()
+    {
+
+    }
+  
+    public void GainInfulence()
+    {
+
+    }
+
+    public void Sacrifice()
+    {
+        
+    }
+
+    
+
+
+
+}
