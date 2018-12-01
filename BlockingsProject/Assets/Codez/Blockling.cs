8164b9e0884e84d255bd102d151b47bbd46ffb3f
AbroadGames <AbroadGames@users.noreply.github.com>
2018-12-01 11:00:46 +0000

    Basic Movement

--- BlockingsProject/Assets/Codez/Blockling.cs
+++ BlockingsProject/Assets/Codez/Blockling.cs
@@ -6,16 +6,19 @@ public class Blockling : MonoBehaviour
 {
 
     //Vars
-    [SerializeField] private int speed = 5;
+    [SerializeField] private LayerMask layerMask;
+
+    private int speed = 3;
     [SerializeField] private bool hasPlayerInfulence = false;
     [SerializeField] private bool isSacrificed = false;
 
+    private Rigidbody rb;
 
     // Use this for initialization
     void Start ()
     {
-		
-	}
+        rb = GetComponent<Rigidbody>();
+	} 
 	
 	// Update is called once per frame
 	void Update ()
@@ -23,10 +26,30 @@ public class Blockling : MonoBehaviour
 		
 	}
 
+    void FixedUpdate()
+    {
+        Moving();
+    }
+
 
     private void Moving()
     {
+       
+        rb.MovePosition(transform.position + transform.right * speed * Time.deltaTime);
 
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, -transform.right, out hit, 0.5f ,layerMask))
+        {
+            speed = 3;
+        }
+
+        else if (Physics.Raycast(transform.position, transform.right, out hit, 0.5f,layerMask))
+        { 
+            speed = -3;
+        }
+
+       
     }
 
     private void Stop()
