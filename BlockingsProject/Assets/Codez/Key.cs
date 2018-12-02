251d8a7682b5d2cdc0f56803a8f5d032b00b9f20
AbroadGames <AbroadGames@users.noreply.github.com>
2018-12-02 17:15:35 +0000

    Key / Door Code

---
+++ BlockingsProject/Assets/Codez/Key.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour
+{
+    [SerializeField] private GameObject door;
+    [SerializeField] private AudioSource aud;
+    [SerializeField] private AudioSource aud2;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        aud.Play();
+        Destroy(door,0.5f); 
+        aud2.Play(30000);
+        Destroy(this);
+        Destroy(this.gameObject.GetComponent<Renderer>());
+    }
+}
